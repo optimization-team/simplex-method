71652a0d5fb6a01c83cf337eab445e0bfd8abd80
Alexandra Vabnits <a.vabnits@innopolis.university>
2023-10-05 23:08:15 +0300

    refactoring

--- test_simplex.py
+++ test_simplex.py
@@ -1,5 +1,7 @@
 """
-Testing Simplex class
+Tests can be run with command: pytest
+Tests are located in tests folder
+
 """
 import os
 import pytest
@@ -23,20 +25,29 @@ class SimplexTestCase:
         optimal function value
     """
 
-    def __init__(self, function: Function, matrix: matrix, b: array, approximation: int | float,
-                 x: list[float], opt: int | float):
+    def __init__(
+        self,
+        function: Function,
+        matrix: matrix,
+        b: array,
+        approximation: int | float,
+        x: list[float],
+        opt: int | float,
+    ):
         self.simplex = Simplex(function, matrix, b, approximation)
         self.x = x
         self.opt = opt
         self.approximation = approximation
 
     def __str__(self):
-        return f'TestCase:\n{self.simplex.function},\n' \
-               f'A:\n{self.simplex.A},\n' \
-               f'b: {self.simplex.b},\n' \
-               f'accuracy: {self.approximation},\n' \
-               f'Vector of decision variables: ({", ".join(map(str, self.x))}),\n' \
-               f'Optimal value of objection function: {self.opt}'
+        return (
+            f"TestCase:\n{self.simplex.function},\n"
+            f"A:\n{self.simplex.A},\n"
+            f"b: {self.simplex.b},\n"
+            f"accuracy: {self.approximation},\n"
+            f'Vector of decision variables: ({", ".join(map(str, self.x))}),\n'
+            f"Optimal value of objection function: {self.opt}"
+        )
 
 
 class TestSimplex:
@@ -53,16 +64,15 @@ class TestSimplex:
     Methods
     ----------
     test_simplex(test_file)
-        test simplex method on a test case with parametrization on list of test cases
+        testing simplex method on a test case with parametrization on list of test cases
     """
-    tests = os.listdir('tests')
-    test_cases = [os.path.join('tests', file) for file in tests]
+
+    tests = os.listdir("tests")
+    test_cases = [os.path.join("tests", file) for file in tests]
 
     @pytest.mark.parametrize("test_file", test_cases)
     def test_simplex(self, test_file):
         """
-        test the Simplex method with various test cases
-
         Parameters
         ----------
         test_file : str
@@ -79,7 +89,8 @@ class TestSimplex:
         opt, x = testcase.simplex.plug_optimize()
 
         for i, val in enumerate(x):
-            assert round(val, testcase.approximation) == \
-                   round(testcase.x[i], testcase.approximation)
+            assert round(val, testcase.approximation) == round(
+                testcase.x[i], testcase.approximation
+            )
 
         assert opt == testcase.opt
